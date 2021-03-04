ec40eed757d2f7302db9ec38199a895984960677
Peter Li <peterli1234@126.com>
2021-03-03 17:48:02 -0800

    implement md5 encryption salt adding

--- src/main/java/com/mmall/util/MD5Util.java
+++ src/main/java/com/mmall/util/MD5Util.java
@@ -43,6 +43,7 @@ public class MD5Util {
     }
 
     public static String MD5EncodeUtf8(String origin) {
+        origin = origin + PropertiesUtil.getProperty("password.salt", "");
         return MD5Encode(origin, "utf-8");
     }
 
