642e015891a2d28e608630e88aeda7e4ab22ab89
cutePanda123 <peterli1234@126.com>
2021-03-16 19:43:05 -0700

    fixed endpoint urls in the file upload testing website

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -19,7 +19,7 @@ public class FileServiceImpl implements IFileService {
     public String upload(MultipartFile file, String path) {
         String fullFileName = file.getOriginalFilename();
         int dotPos = fullFileName.lastIndexOf(".");
-        String fileExtensionName = dotPos >=0 ? fullFileName.substring(dotPos + 1) : "";
+        String fileExtensionName = dotPos >= 0 ? fullFileName.substring(dotPos + 1) : "";
         String fileName = dotPos >= 0 ? fullFileName.substring(0, dotPos) : fullFileName;
         String serverFileName = UUID.randomUUID().toString() + "." + fileExtensionName;
         logger.info("Upload file name {}, path {}, server file name {}", fileName, path, serverFileName);
@@ -30,15 +30,16 @@ public class FileServiceImpl implements IFileService {
             fileDir.mkdirs();
         }
         File targetFile = new File(path, serverFileName);
-
+        String targetFileName = targetFile.getName();;
         try {
             file.transferTo(targetFile);
             FtpUtil.uploadFiles(Lists.newArrayList(targetFile));
-            targetFile.delete();
         } catch (Exception e) {
             logger.error("file upload exception", e);
-            return null;
+            targetFileName = null;
+        } finally {
+            targetFile.delete();
         }
-        return targetFile.getName();
+        return targetFileName;
     }
 }
