3a430816f7d47391b05657caa75bd6f9643058b6
cutePanda123 <peterli1234@126.com>
2021-03-19 21:19:01 -0700

    initiated shipping controller

---
+++ src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.mmall.service.impl;
+
+import com.mmall.service.IShippingService;
+import org.springframework.stereotype.Service;
+
+@Service("iShippingService")
+public class ShippingServiceImpl implements IShippingService  {
+}
