dc5ec05ff8195e959df7f4a93dff78f78fdcfd85
cutePand123 <peterli1234@126.com>
2021-04-03 07:22:36 -0700

    init alipay callback endpoint

--- src/main/java/com/mmall/controller/portal/OrderController.java
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -1,10 +1,13 @@
 package com.mmall.controller.portal;
 
+import com.google.common.collect.Maps;
 import com.mmall.common.Constants;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IOrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +16,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Iterator;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/order/")
 public class OrderController {
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
     @Autowired
     private IOrderService iOrderService;
 
@@ -30,5 +37,25 @@ public class OrderController {
         String path = request.getSession().getServletContext().getRealPath(Constants.IMAGE_UPLOAD_DIR);
 
         return iOrderService.pay(orderNum, user.getId(), path);
+    }
+
+    @RequestMapping(value = "alipay_callback.do")
+    @ResponseBody
+    public Object alipayCallback(HttpServletRequest request) {
+        Map<String, String> params = Maps.newHashMap();
+
+        Map requestParams = request.getParameterMap();
+        for (Iterator it = requestParams.keySet().iterator(); it.hasNext();) {
+            String name = (String)it.next();
+            String[] values = (String[]) requestParams.get(name);
+            StringBuffer buffer = new StringBuffer();
+            for (int i = 0; i < values.length; ++i) {
+                buffer.append(i == values.length - 1 ? values[i] : values[i] + ",");
+            }
+            params.put(name, buffer.toString());
+        }
+        logger.info("");
+
+        return null;
     }
 }
