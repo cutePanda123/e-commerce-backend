986c5dbc39a661e9506a0ab223a3a23214a5bcc7
cutePand123 <peterli1234@126.com>
2021-04-24 14:36:30 -0700

    added config to redis connection pool

--- src/main/java/com/mmall/common/RedisConnectionPool.java
+++ src/main/java/com/mmall/common/RedisConnectionPool.java
@@ -1,4 +1,15 @@
 package com.mmall.common;
 
+import com.mmall.util.PropertiesUtil;
+import redis.clients.jedis.JedisPool;
+
 public class RedisConnectionPool {
+    private static JedisPool pool;
+
+    private static Integer maxTotal = Integer.parseInt(PropertiesUtil.getProperty("redis.max.total", "20"));
+    private static Integer maxIdx = Integer.parseInt(PropertiesUtil.getProperty("redis.max.idle", "10"));
+    private static Integer minIdx = Integer.parseInt(PropertiesUtil.getProperty("redis.min.idle", "10"));
+
+    private static Boolean testOnBorrow = Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.borrow", "true"));
+    private static Boolean testOnReturn = Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.return", "true"));
 }
