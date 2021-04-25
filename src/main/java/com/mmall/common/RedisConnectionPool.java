1149286cfa5680446d7458c08a7eed26c6f2c7d7
cutePand123 <peterli1234@126.com>
2021-04-24 17:45:15 -0700

    implemented and tested redis connection pool

--- src/main/java/com/mmall/common/RedisConnectionPool.java
+++ src/main/java/com/mmall/common/RedisConnectionPool.java
@@ -1,15 +1,56 @@
 package com.mmall.common;
 
 import com.mmall.util.PropertiesUtil;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisConnectionPool {
     private static JedisPool pool;
 
     private static Integer maxTotal = Integer.parseInt(PropertiesUtil.getProperty("redis.max.total", "20"));
-    private static Integer maxIdx = Integer.parseInt(PropertiesUtil.getProperty("redis.max.idle", "10"));
-    private static Integer minIdx = Integer.parseInt(PropertiesUtil.getProperty("redis.min.idle", "10"));
+    private static Integer maxIdle = Integer.parseInt(PropertiesUtil.getProperty("redis.max.idle", "10"));
+    private static Integer minIdle = Integer.parseInt(PropertiesUtil.getProperty("redis.min.idle", "10"));
 
     private static Boolean testOnBorrow = Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.borrow", "true"));
     private static Boolean testOnReturn = Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.return", "true"));
+
+    private static String redisIp = PropertiesUtil.getProperty("redis.ip");
+    private static Integer redisPort = Integer.parseInt(PropertiesUtil.getProperty("redis.port"));
+
+    private static void initPool() {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(maxTotal);
+        config.setMaxIdle(maxIdle);
+        config.setMinIdle(minIdle);
+        config.setTestOnBorrow(testOnBorrow);
+        config.setTestOnReturn(testOnReturn);
+        config.setBlockWhenExhausted(true);
+
+        pool = new JedisPool(config, redisIp, redisPort, 1000 * 2);
+    }
+
+    static {
+        initPool();
+    }
+
+    public static Jedis getJedis() {
+        return pool.getResource();
+    }
+
+    public static void returnBrokenResource(Jedis jedis) {
+        pool.returnBrokenResource(jedis);
+    }
+
+    public static void returnResource(Jedis jedis) {
+        pool.returnResource(jedis);
+    }
+
+    public static void main(String[] args) {
+        Jedis jedis = pool.getResource();
+        jedis.set("panda123", "pandavalue123");
+        returnResource(jedis);
+        pool.destroy();
+        System.out.println("jedis pool test finished");
+    }
 }
