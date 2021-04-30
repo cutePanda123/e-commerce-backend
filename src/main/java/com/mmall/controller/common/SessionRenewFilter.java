670e6aca88ac0c7f14496b08dd011acf0fa92c24
cutePand123 <peterli1234@126.com>
2021-04-29 21:38:03 -0700

    implemented session renew filter

---
+++ src/main/java/com/mmall/controller/common/SessionRenewFilter.java
@@ -0,0 +1,38 @@
+package com.mmall.controller.common;
+
+import com.github.pagehelper.StringUtil;
+import com.mmall.common.Constants;
+import com.mmall.pojo.User;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisUtil;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class SessionRenewFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+        if (StringUtil.isNotEmpty(loginToken)) {
+            String userJsonStr = RedisUtil.get(loginToken);
+            User user = JsonUtil.str2obj(userJsonStr, User.class);
+            if (user != null) {
+                RedisUtil.expire(loginToken, Constants.RedisCacheExpirationTime.REDIS_SESSION_EXPIRATION_TIME);
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
