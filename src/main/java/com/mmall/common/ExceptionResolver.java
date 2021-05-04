a7dfbd3847427f0b6620e4053d08b6f103f5cd5a
cutePand123 <peterli1234@126.com>
2021-05-04 09:41:17 -0700

    implmented global exception handler

---
+++ src/main/java/com/mmall/common/ExceptionResolver.java
@@ -0,0 +1,26 @@
+package com.mmall.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class ExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        log.error("{} Exception: ", httpServletRequest.getRequestURI(), e);
+
+        // need to upgrade jackson1.x to 2.x, then MappingJackson2JsonView can be used
+        ModelAndView modelAndView = new ModelAndView(new MappingJacksonJsonView());
+        modelAndView.addObject("status", ResponseCode.ERROR.getCode());
+        modelAndView.addObject("msg", "API exception. More details can be found in server log");
+        modelAndView.addObject("data", e.toString());
+        return modelAndView;
+    }
+}
