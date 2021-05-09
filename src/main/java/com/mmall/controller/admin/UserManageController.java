6886ff97936edd0aea1839e89ac3433c89471559
cutePand123 <peterli1234@126.com>
2021-05-08 18:36:47 -0700

    refactor admain login with redis

--- src/main/java/com/mmall/controller/admin/UserManageController.java
+++ src/main/java/com/mmall/controller/admin/UserManageController.java
@@ -1,15 +1,20 @@
 package com.mmall.controller.admin;
 
 import com.mmall.common.Constants;
+import com.mmall.common.RedisShardedPool;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisShardedPoolUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -20,12 +25,22 @@ public class UserManageController {
 
     @RequestMapping(value = "login.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session) {
+    public ServerResponse<User> login(
+            String username,
+            String password,
+            HttpSession session,
+            HttpServletResponse httpServletResponse) {
         ServerResponse<User> response = iUserService.login(username, password);
         if (response.isSuccess()) {
             User user = response.getData();
             if (user.getRole() == Constants.Role.ROLE_ADMIN) {
-                session.setAttribute(Constants.CURRENT_USER, user);
+                //session.setAttribute(Constants.CURRENT_USER, user);
+                CookieUtil.writeLoginToken(httpServletResponse, session.getId());
+                RedisShardedPoolUtil.setEx(
+                        session.getId(),
+                        JsonUtil.obj2str(response.getData()),
+                        Constants.RedisCacheExpirationTime.REDIS_SESSION_EXPIRATION_TIME
+                );
                 return response;
             }
             return ServerResponse.createByErrorMessage("user is not a admin");
