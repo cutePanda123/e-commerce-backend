a64214bd6684518e0c3c8f53dba4bc4fe2888983
cutePand123 <peterli1234@126.com>
2021-05-15 07:30:27 -0700

    added a simple cronjob to close order periotically

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -1,8 +1,10 @@
 package com.mmall.task;
 
 import com.mmall.service.IOrderService;
+import com.mmall.util.PropertiesUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,4 +12,12 @@ import org.springframework.stereotype.Component;
 public class CloseOrderTask {
     @Autowired
     IOrderService iOrderService;
+
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void closeOrderTaskV1() {
+        log.info("close order cronjob start");
+        int timeout = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour", "2"));
+        iOrderService.closeOrders(timeout);
+        log.info("close order cronjob end");
+    }
 }
